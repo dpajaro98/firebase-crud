ffaac2400f2df1e8737cd0b1b5236425426a3390
dpajaro98 <drpajaro7@misena.edu.co>
2022-09-24 19:02:32 -0500

    este si

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../DBConfig/supabase";
-import { useMeds } from "../context/MedContext";
+
 import { Link } from "react-router-dom";
 import ShowSupabase from "../components/ShowSupabase";
 
